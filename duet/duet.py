c22dff1f7f2eadf76a67cab0168da06bf2d048de
Jack Smith <jsmithtech2019@gmail.com>
2020-02-13 20:12:49 -0600

    updoots

--- duet/duet.py
+++ duet/duet.py
@@ -1,29 +1,83 @@
 from flask import request, url_for
 from flask_api import FlaskAPI, status, exceptions
 import mysql.connector
+import json
 
 app = FlaskAPI(__name__)
 
 @app.route("/hello-world")
-def hello_world():
+def hello_world(dealership):
     return 'Hello world from DUET!'
 
 # This is how the endpoint will receive data from controller applications
 @app.route('/upload/', methods=['POST'])
 def upload_data():
-    # Parse data from body of request
-    post_body = request.json
+    # Check if valid JSON
+    if not request.is_json:
+        return 'Missing JSON in request', status.HTTP_400_BAD_REQUEST
+        #return '', status.HTTP_400_BAD_REQUEST
 
+    # With valid JSON, parse data from body of request
+    content = request.get_json()
 
+    # Parse dealership information
+    dealership_check(request.json['dealership'])
 
+    # Parse Employee information
+    employee_check(request.json['employee'])
+
+    ## At this point everything is good, parse and evaluate
+    # Parse customer information
+    customer = request.json['customer']
+
+    # Parse test information
+    truck = request.json['results']['truck']
+    trailer = request.json['results']['trailer']
+
+    # TODO: do things
+    # return jsonify({"msg": "Missing JSON in request"}), 400
+    return '', status.HTTP_201_CREATED
 
 # Returns test results for specified customer in JSON
 @app.route('/download/', methods=['POST'])
 def download_data():
+    return '', status.HTTP_202_ACCEPTED
 
+def dealership_check(data):
+    # Check ID matches dealership
+    dealership_id = data['id']
+    #if not mysql.checkexists(dealership_id):
+    #    return '', status.HTTP_401_UNAUTHORIZED
+
+    # Check name matches dealership
+    dealership_name = data['name']
+    #if not mysql.checkexists(dealership_name):
+    #    return '', status.HTTP_401_UNAUTHORIZED
+
+def employee_check(data):
+    # Check employee id exists in database
+    employee_id = data['id']
+    #if not mysql.checkexists(employee_id):
+    #    return '', status.HTTP_401_UNAUTHORIZED
+
+    employee_email = data['']
+    #if not mysql.checkexists(employee_email):
+    #    return '', status.HTTP_401_UNAUTHORIZED
+
+    employee_password = data['password']
+    #if not mysql.checkexists(employee_password):
+    #    return '', status.HTTP_401_UNAUTHORIZED
+
+    employee_phone = data['phone']
+    #if not mysql.checkexists(employee_phone):
+    #    return '', status.HTTP_401_UNAUTHORIZED
+
+    module_id = data['module']
+    #if not mysql.checkexists(module_id):
+    #    return '', status.HTTP_401_UNAUTHORIZED
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
 
 
 
@@ -32,8 +86,6 @@ if __name__ == "__main__":
 #         'url': request.host_url.rstrip('/') + url_for('notes_detail', key=key),
 #         'text': notes[key]
 #     }
-
-
 
 # @app.route("/", methods=['GET', 'POST'])
 # def notes_list():
