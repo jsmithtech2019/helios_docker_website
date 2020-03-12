661f3997d4a0ec8f22395869ce69a0df390a6f69
Jack Smith <jsmithtech2019@gmail.com>
2020-03-12 01:17:18 -0500

    yeah good luck truly

---
+++ code/pages/results.php
@@ -0,0 +1,91 @@
+<!-- ./var/www/html/pages/results.php -->
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <!-- not sure what this one ^^ does... all 3 are needed to come first?-->
+
+    <!-- Bootstrap Core CSS -->
+    <link href="/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom CSS: You can use this stylesheet to override any Bootstrap styles and/or apply your own styles -->
+    <link href="/css/custom.css" rel="stylesheet">
+
+    <!-- Custom Fonts from Google -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <title>Test Results Portal</title>
+</head>
+
+<body>
+    <!-- Navigation -->
+    <nav id="siteNav" class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Logo and responsive toggle -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/">
+                    <span class="glyphicon glyphicon-fire"></span>
+                    TEST RESULTS
+                </a>
+            </div>
+            <!-- Navbar links -->
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li class="active">
+                        <a href="/pages/results.php">Results</a>
+                    </li>
+                    <li>
+                        <a href="/pages/admin.php">Administration</a>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Documentation<span class="caret"></span></a>
+                        <ul class="dropdown-menu" aria-labelledby="about-us">
+                            <li><a href="/documentation/webserver/index.html">Server</A></li>
+                            <li><a href="/documentation/duet/index.html">DUET</A></li>
+                            <li><a href="/documentation/controller-application/index.html">Flutter</a></li>
+                            <li><a href="/documentation/rtos/index.html">TI-RTOS</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="/pages/contact.html">Contact</a>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container -->
+    </nav>
+
+    <!-- Header -->
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <h1>Results Portal</h1>
+                <?php include('../db.php');?>
+            </div>
+        </div>
+    </header>
+
+    <!-- all this nonsense is for handling animations of the header from what I can tell... -->
+    <!-- jQuery -->
+    <script src="/js/jquery-1.11.3.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="/js/jquery.easing.min.js"></script>
+
+    <!-- Custom Javascript -->
+    <script src="/js/custom.js"></script>
+
+</body>
+</html>
